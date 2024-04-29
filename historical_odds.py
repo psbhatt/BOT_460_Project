b6cb1bd38f16ef631d38f01226fe3777c434de49
lukeolsen06 <lukeolsen06@gmail.com>
2024-04-28 21:43:12 -0400

    updated historical_odds.py

--- historical_odds.py
+++ historical_odds.py
@@ -12,8 +12,9 @@ DATE_FORMAT = 'iso'
 
 # Define the start and end dates
 start_date = datetime(2023, 10, 24)
-end_date = datetime(2024, 3, 25)
+end_date = datetime(2024, 4, 14)
 
+# Initialize start date
 current_date = start_date
 
 # List to hold extracted data
@@ -21,7 +22,7 @@ events = []
 
 while current_date <= end_date:
     # Convert current date to the desired format
-    current_date_str = current_date.strftime('%Y-%m-%dT00:00:00Z')
+    current_date_str = current_date.strftime('%Y-%m-%dT12:00:00Z')
 
     # Make API request for the current date
     response = requests.get(
@@ -38,24 +39,29 @@ while current_date <= end_date:
 
     if response.status_code == 200:
         odds_data = response.json()
+        # Check if there is a 'X-RateLimit-Remaining' header
+        remaining_requests = response.headers.get('X-RateLimit-Remaining')
+        if remaining_requests is not None:
+            print(f'Remaining requests: {remaining_requests}')
         # Process the odds data
         for event in odds_data['data']:
-            for bookmaker in event['bookmakers']:
-                if bookmaker['key'] == 'draftkings':
-                    for market in bookmaker['markets']:
-                        if market['key'] == MARKET:
-                            for outcome in market['outcomes']:
-                                home_team = event['home_team']
-                                away_team = event['away_team']
-                                team = outcome['name']
-                                price = outcome['price']
-                                events.append({
-                                    'Date': current_date_str,
-                                    'Matchup': f"{home_team} vs {away_team}",
-                                    'Bookmaker': bookmaker['key'],
-                                    'Team': team,
-                                    'Moneyline Odds': price
-                                })
+            if (datetime.strptime(event['commence_time'], "%Y-%m-%dT%H:%M:%SZ") - timedelta(hours=4, minutes=0)).date() == current_date.date():
+                    for bookmaker in event['bookmakers']:
+                        if bookmaker['key'] == 'draftkings':
+                            for market in bookmaker['markets']:
+                                if market['key'] == MARKET:
+                                    for outcome in market['outcomes']:
+                                        home_team = event['home_team']
+                                        away_team = event['away_team']
+                                        team = outcome['name']
+                                        price = outcome['price']
+                                        events.append({
+                                            'Date': current_date_str,
+                                            'Matchup': f"{home_team} vs {away_team}",
+                                            'Bookmaker': bookmaker['key'],
+                                            'Team': team,
+                                            'Moneyline Odds': price
+                                        })
 
     else:
         print(f'Failed to fetch data for {current_date_str}')
